afa5c5923c6fb5a6c71b76e61b3f07e7924c3c1b
罗成 <luocheng@luochengdeMacBook-Pro.local>
2020-01-29 21:17:49 +0800

    test

--- src/detector.c
+++ src/detector.c
@@ -977,7 +977,7 @@ float quantize_detector_map(char *datacfg, char *cfgfile, char *weightfile, floa
     int i = 0;
     int t;
 
-    m = 800;
+    m = 2000;
 
     const float thresh = .005;
     const float nms = .45;
@@ -1170,7 +1170,7 @@ float quantize_detector_map(char *datacfg, char *cfgfile, char *weightfile, floa
             free_image(val[t]);
             free_image(val_resized[t]);
         }
-        if(i%200 == 0)
+        if(i%400 == 0)
             rewrite_cfg(net, cfgfile);
     }
 
