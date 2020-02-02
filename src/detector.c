cbd5cdee8f26b0ed7d683e4f7dd7991c69d45cfb
罗成 <luocheng@luochengdeMacBook-Pro.local>
2020-02-02 20:23:05 +0800

    update

--- src/detector.c
+++ src/detector.c
@@ -1107,7 +1107,7 @@ float quantize_detector_map(char *datacfg, char *cfgfile, char *weightfile, floa
     }
     time_t start = time(0);
     for (i = nthreads; i < m + nthreads; i += nthreads) {
-        if(i % 1000 == nthreads)
+        if(i % 200 == nthreads)
         {
             int finish = quantized_network(net);
             if(finish) break;
@@ -1119,14 +1119,14 @@ float quantize_detector_map(char *datacfg, char *cfgfile, char *weightfile, floa
             val_resized[t] = buf_resized[t];
         }
         for (t = 0; t < nthreads && (i + t) < m; ++t) {
-            const int image_index = (i + t) % 1000;
+            const int image_index = (i + t) % 200;
             args.path = paths[image_index];
             args.im = &buf[t];
             args.resized = &buf_resized[t];
             thr[t] = load_data_in_thread(args);
         }
         for (t = 0; t < nthreads && i + t - nthreads < m; ++t) {
-            const int image_index = (i + t - nthreads) % 1000;
+            const int image_index = (i + t - nthreads) % 200;
             char *path = paths[image_index];
             char *id = basecfg(path);
             float *X = val_resized[t].data;
