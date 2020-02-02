2320754e6ddc4a0047088ef9b3e4b7794432a0e6
罗成 <luocheng@luochengdeMacBook-Pro.local>
2020-02-02 15:12:05 +0800

    update

--- src/convolutional_kernels.cu
+++ src/convolutional_kernels.cu
@@ -428,8 +428,11 @@ void forward_convolutional_layer_gpu(convolutional_layer l, network_state state)
             for(int i = 0; i < l.c*l.h*l.w*l.batch; i++)
             {
                 if(delta_max < abs(l.fix_input[i]))
-                    printf("no %d %d %f %f %f\n", i, l.c*l.h*l.w*l.batch, delta_max, abs(l.fix_input[i]), l.fix_input[i]);
+                    delta_max = abs(l.fix_input[i]);
             }
+
+            if(delta_max > *l.max_value_in)
+                 *l.max_value_in = delta_max;
         }
 
         if(l.quantized_switch & 2)
