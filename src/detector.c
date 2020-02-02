8c8ad38f6a0d833a230305ae6536f8cb32a02476
罗成 <luocheng@luochengdeMacBook-Pro.local>
2020-02-02 10:19:15 +0800

    update

--- src/detector.c
+++ src/detector.c
@@ -1014,7 +1014,7 @@ float quantize_detector_map(char *datacfg, char *cfgfile, char *weightfile, floa
     int i = 0;
     int t;
 
-    m = 5000;
+    m = 100;
 
     const float thresh = .005;
     const float nms = .45;
@@ -1207,7 +1207,7 @@ float quantize_detector_map(char *datacfg, char *cfgfile, char *weightfile, floa
             free_image(val[t]);
             free_image(val_resized[t]);
         }
-        if(i%5000 == 0)
+        if(i%100 == 0)
         {
             rewrite_cfg(net, cfgfile);
             char buff[1024];
